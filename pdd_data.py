3eddda282dc78bb44eb3e1eb5f6d06cb80fe72ed
KushanRaj <be19b004@smail.iitm.ac.in>
2024-01-05 18:53:08 +0530

    Hydra fix

--- pdd_data.py
+++ pdd_data.py
@@ -1,10 +1,26 @@
+"""Data Module for loading PDF documents"""
 from transformers import DonutProcessor, VisionEncoderDecoderModel
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from utils import *
 import pandas as pd
 import os
+from typing import List, Dict, Any
 
-def load_and_preprocess_csv(csv_path):
+def load_and_preprocess_csv(csv_path : str) -> pd.DataFrame:
+    """
+    Load and preprocess the CSV file containing the metadata for PDF documents.
+
+    Args:
+    csv_path (str): The file path to the CSV file.
+
+    Returns:
+    DataFrame: A Pandas DataFrame with preprocessed data.
+
+    The function performs the following preprocessing steps:
+    - Loads data from the specified CSV file.
+    - Merges specific columns into arrays for easier access and manipulation.
+    - Drops the original year columns after merging.
+    """
     # Load CSV
     df = pd.read_csv(csv_path)
 
@@ -25,18 +41,50 @@ def load_and_preprocess_csv(csv_path):
 
     return df
 
-# Custom Dataset
 class PDFDocumentDataset(Dataset):
-    def __init__(self, csv_path, pdf_folder_path, processor, model):
+    """
+    A custom Dataset class for handling PDF documents.
+
+    This class is designed to work with PDF files, extracting information using a Donut model,
+    and generating text summaries as targets. It supports processing of multiple pages of a PDF,
+    unlike standard approaches which often focus on single-page processing.
+
+    Attributes:
+    csv_path (str): Path to the CSV file containing document metadata.
+    pdf_folder_path (str): Folder path containing PDF documents.
+    processor (DonutProcessor): Instance of DonutProcessor for processing documents.
+    model (VisionEncoderDecoderModel): Pretrained model for generating embeddings.
+    dataframe (DataFrame): DataFrame holding the preprocessed data.
+    """
+
+    def __init__(self, csv_path : str, pdf_folder_path : str, processor : DonutProcessor, model : VisionEncoderDecoderModel) -> None:
+        """
+        Initialize the dataset with CSV and PDF paths, and the processor and model.
+
+        Args:
+        csv_path (str): Path to the CSV file containing document metadata.
+        pdf_folder_path (str): Folder path containing PDF documents.
+        processor (DonutProcessor): Instance of DonutProcessor.
+        model (VisionEncoderDecoderModel): Instance of a pretrained model.
+        """
         self.pdf_folder_path = pdf_folder_path
         self.processor = processor
         self.model = model
         self.dataframe = self.check_data(load_and_preprocess_csv(csv_path))
 
-    def check_data(self, dataframe):
+    def check_data(self, dataframe : pd.DataFrame) -> List[pd.DataFrame]:
+        """
+        Validate and filter the DataFrame based on the existence of corresponding PDF files.
 
+        Args:
+        dataframe (DataFrame): The preprocessed DataFrame.
+
+        Returns:
+        list: A filtered list of DataFrame rows where the PDF files exist.
+        """
         df2 = []
 
+        # Check for the existence of each PDF and append the row to df2 if the PDF exists
         for _, row in dataframe.iterrows():
             pdf_path = f"{self.pdf_folder_path}/{row['Project ID']}.pdf"
             if os.path.exists(pdf_path):
@@ -44,17 +92,36 @@ class PDFDocumentDataset(Dataset):
 
         return df2
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Return the length of the dataset.
+
+        Returns:
+        int: The number of items in the dataset.
+        """
         return len(self.dataframe)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx : int) -> Dict[str, Any]:
+        """
+        Retrieve a single item from the dataset by its index.
+
+        Args:
+        idx (int): The index of the item to retrieve.
+
+        Returns:
+        dict: A dictionary containing the data for the item, including embeddings and tokenized text.
+        """
+        # Get the row and its corresponding PDF path
         row = self.dataframe[idx]
         pdf_path = f"{self.pdf_folder_path}/{row['Project ID']}.pdf"
+
+        # Generate embeddings from the PDF
         embeddings = get_concatenated_representation(pdf_path, self.processor, self.model)
 
         # Create a comma-separated text from the row, excluding embeddings-related data
-        text_data = '\n'.join([f"{k}:{v}" for k,v in row.drop(['Project ID', 'vintage_issue', 'retired_credits']).to_dict().items()])#.to_csv(header=False, index=False).strip('\n')
+        text_data = '\n'.join([f"{k}:{v}" for k,v in row.drop(['Project ID', 'vintage_issue', 'retired_credits']).to_dict().items()])
 
+        # Construct the data dictionary
         data = {
             "project_id": row['Project ID'],
             "description": text_data,
@@ -63,27 +130,19 @@ class PDFDocumentDataset(Dataset):
             "embeddings": embeddings
         }
 
+        # Concatenate data fields into a single text string, excluding embeddings
         text = ""
         for k,v in data.items():
-            if k!= 'embeddings':
+            if k != 'embeddings':
                 text += f"{k} : {v},"
 
-        out = self.processor.tokenizer(text, padding = "max_length", max_length = 1024, add_special_tokens=False, return_tensors="pt")
-
+        # Tokenize the text
+        out = self.processor.tokenizer(text, padding="max_length", max_length=1024, add_special_tokens=False, return_tensors="pt")
         labels, attn_mask = out.input_ids, out.attention_mask
 
+        # Add labels and attention mask to the data dictionary
         data["labels"] = labels
         data["attn_mask"] = attn_mask
-        # Prepare the output dictionary
+
+        # Return the prepared data dictionary
         return data
-    
-if __name__ == '__main__':
-
-    # Initialize the processor and model
-    processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
-    model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
-    model.to("cuda" if torch.cuda.is_available() else "cpu")
-    pass
-
-    dset = PDFDocumentDataset("clean_data.csv", "dataset", processor, model)
-    print(dset[0])
